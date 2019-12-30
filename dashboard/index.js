225e0150a3ba58afaa93932f2a63a64caf513b5e
Gavin Mogan <git@gavinmogan.com>
2019-12-30 14:32:57 -0800

    make all the tests happy and close up after themselves

--- dashboard/index.js
+++ dashboard/index.js
@@ -1,25 +1,26 @@
 const express = require('express');
 
+const path = require('path');
 const debug = require('debug')('irc-dashboard');
-const app = express();
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-
-app.get('/', function(req, res) {
-  const clients = Object.keys(app.irc.clients).map(function(uuid) { return app.irc.clients[uuid]; });
-  res.render('clients', {clients: clients});
-});
-
-app.get('/users/:uuid', function(req, res) {
-  const client = app.irc.clients[req.params.uuid];
-  if (!client) return res.status(404).send('Not Found');
-  res.render('client', {client: client});
-});
 
 
 function hook(ircServer, port) {
+  const app = express();
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'jade');
+
+  app.get('/', function(req, res) {
+    const clients = Object.keys(app.irc.clients).map(function(uuid) { return app.irc.clients[uuid]; });
+    res.render('clients', {clients: clients});
+  });
+
+  app.get('/users/:uuid', function(req, res) {
+    const client = app.irc.clients[req.params.uuid];
+    if (!client) return res.status(404).send('Not Found');
+    res.render('client', {client: client});
+  });
   app.irc = ircServer;
-  app.listen(port, function(err) {
+  return app.listen(port, function(err) {
     debug('Dashboard listening on ' + port);
   });
 }
